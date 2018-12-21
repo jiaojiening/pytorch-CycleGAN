af7eee97bd122c54ec93b3ba38130eff3d7c8f3e
jiaojiening <jiaojn@mail2.sysu.edu.cn>
2018-12-21 10:18:21 +0800

    add psnr ssim evaluation

--- models/test_reid_SR_model.py
+++ models/test_reid_SR_model.py
@@ -20,7 +20,7 @@ class TestReidSRModel(BaseModel):
         # specify the training losses you want to print out. The program will call base_model.get_current_losses
         self.loss_names = []
         # specify the images you want to save/display. The program will call base_model.get_current_visuals
-        self.visual_names = ['real_B', 'fake_A']
+        self.visual_names = ['real_B', 'fake_A', 'GT_B']
         # specify the models you want to save to the disk. The program will call base_model.save_networks and base_model.load_networks
         self.model_names = ['G_B', 'D_reid']
 
@@ -42,19 +42,31 @@ class TestReidSRModel(BaseModel):
         self.image_paths = input['img_paths']  # list
         # for SR real_B
         self.img_attr = input['img_attr'].to(self.device)
+        self.GT_B = input['GT_img'].to(self.device)
 
     def SR_B(self):
         self.real_B = self.img
+        self.B_real_attr = self.img_attr
 
         # combine the attributes
-        B_real_attr = torch.unsqueeze(self.img_attr, 2)
+        B_real_attr = torch.unsqueeze(self.B_real_attr, 2)
         B_real_attr = B_real_attr.repeat(1, 1, self.real_B.size()[2] * self.real_B.size()[3])
         B_real_attr = torch.reshape(B_real_attr, (-1, self.num_attr, self.real_B.size()[2], self.real_B.size()[3]))
         B_real_attr = B_real_attr.float()
         comb_input_real = torch.cat([B_real_attr, self.real_B], 1)
 
         self.fake_A = self.netG_B(comb_input_real)
+        # set the fake_A for reid
         self.img = self.fake_A
+
+    def psnr_eval(self):
+        # compute the PSNR for the test
+        self.bicubic_psnr = networks.compute_psnr(self.GT_B, self.real_B)
+        self.psnr = networks.compute_psnr(self.GT_B, self.fake_A)
+
+    def ssim_eval(self):
+        self.bicubic_ssim = networks.compute_ssim(self.GT_B, self.real_B)
+        self.ssim = networks.compute_ssim(self.GT_B, self.fake_A)
 
     def extract_features(self):
         # Remove the final fc layer and classifier layer
