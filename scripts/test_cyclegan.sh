b63aa6c812f4f15b89dfcbefe41a10e40326e950
jiaojiening <jiaojn@mail2.sysu.edu.cn>
2018-12-21 11:24:17 +0800

    record

--- scripts/test_cyclegan.sh
+++ scripts/test_cyclegan.sh
@@ -6,38 +6,35 @@ python test.py --dataroot ./datasets/CelebA --name CelebA_cCyclegan --dataset_mo
 python test.py --dataroot ./datasets/CelebA --name CelebA_SRcCyclegan --dataset_mode CelebA --model SRcCycle_gan --phase test --loadSize 158 --fineSize 128 --gpu 2 --no_dropout --epoch 5
 
 python test_SR.py --dataroot DukeMTMC-reID --name Duke_SRcCyclegan_1 --dataset_mode Duke --model SRcCycle_gan --phase test --gpu 4 --no_dropout
+python test_SR.py --dataroot DukeMTMC-reID --name Duke_SRcCyclegan_1 --dataset_mode single_Duke --model test_SR --phase test --gpu 6 --no_dropout
+python test_SR.py --dataroot DukeMTMC-reID --name Duke_SRcCyclegan --dataset_mode single_Duke --model test_SR --phase test --gpu 6 --no_dropout
+python test_SR.py --dataroot DukeMTMC-reID --name Duke_cCyclegan --dataset_mode single_Duke --model test_SR --phase test --gpu 6 --no_dropout
+python test_SR.py --dataroot DukeMTMC-reID --name Duke_cCyclegan --dataset_mode Duke --model test_SR --gpu 6 --no_dropout --save_phase train
+
+python test_SR.py --dataroot DukeMTMC-reID --name Duke_SRcCyclegan --dataset_mode single_Duke --model test_SR --gpu 6 --no_dropout --save_phase test
+python test_SR.py --dataroot DukeMTMC-reID --name Duke_SRcCyclegan --dataset_mode Duke --model test_SR --gpu 6 --no_dropout --save_phase train
+
+python test_SR.py --dataroot DukeMTMC-reID --name Duke_paired_cCyclegan --dataset_mode single_Duke --model test_SR --gpu 6 --no_dropout --save_phase test
+python test_SR.py --dataroot DukeMTMC-reID --name Duke_paired_cCyclegan --dataset_mode Duke --model test_SR --gpu 6 --no_dropout --save_phase train
+
+python test_SR.py --dataroot DukeMTMC-reID --name Duke_paired_cCyclegan_upscale_8 --dataset_mode single_Duke --model test_SR --gpu 6 --no_dropout --save_phase test --up_scale 8
+python test_SR.py --dataroot DukeMTMC-reID --name Duke_paired_cCyclegan_upscale_8 --dataset_mode Duke --model test_SR --gpu 6 --no_dropout --save_phase train --up_scale 8
 
 
-python test_reid.py --dataroot DukeMTMC-reID --name Duke_Reid --dataset_mode single_Duke --model reid --phase test --gpu 5 --no_dropout --batch_size 2
+
+
+python test_SR.py --dataroot Market --name Market_SRcCyclegan --dataset_mode single_Market --model test_SR --phase test --gpu 6 --no_dropout
+python test_SR.py --dataroot Market --name Market_SRcCyclegan --dataset_mode Market --model test_SR --gpu 6 --no_dropout --save_phase train
 
 python test_reid.py --dataroot Market --name Market_Reid --dataset_mode single_Market --model reid --phase test --gpu 5 --no_dropout --batch_size 50
-
 python test_reid.py --dataroot Market --name Market_Reid_16 --dataset_mode single_Market --model reid --phase test --gpu 5 --no_dropout
-
+python evaluate_reid.py --dataroot Market --name Market_Reid_16
 python test_reid.py --dataroot Market --name Market_Reid_16_step_20 --dataset_mode single_Market --model reid --phase test --gpu 5 --no_dropout
 
-python evaluate_reid.py --dataroot Market --name Market_Reid_16
-
 python test_reid.py --dataroot Market --name Market_Reid_16_lr_0.01 --dataset_mode single_Market --model reid --phase test --gpu 7 --no_dropout
-
 python evaluate_reid.py --dataroot Market --name Market_Reid_16_lr_0.01
 
-python test_reid.py --dataroot Market --name Market_Reid_16_lr_0.01 --dataset_mode single_Market --model reid --phase test --gpu 7 --no_dropout --epoch 100
-
-python evaluate_reid.py --dataroot Market --name Market_Reid_16_lr_0.01 --epoch 100
-
+python test_reid.py --dataroot Market --name Market_Reid_NR_16_lr_0.01 --dataset_mode single_Market --model reid --phase test --gpu 7 --no_dropout --NR
+python evaluate_reid.py --dataroot Market --name Market_Reid_NR_16_lr_0.01
 
 
-python test_reid.py --dataroot DukeMTMC-reID --name Duke_Reid --dataset_mode single_Duke --model reid --phase test --gpu 5 --no_dropout
-
-python test_reid.py --dataroot DukeMTMC-reID --name Duke_Reid --dataset_mode single_Duke --model reid --phase test --gpu 5 --no_dropout --epoch 200
-
-python evaluate_reid.py --dataroot DukeMTMC-reID --name Duke_Reid
-
-python test_reid.py --dataroot DukeMTMC-reID --name Duke_Reid_16 --dataset_mode single_Duke --model reid --phase test --gpu 5 --no_dropout
-
-python evaluate_reid.py --dataroot DukeMTMC-reID --name Duke_Reid_16
-
-python test_reid.py --dataroot DukeMTMC-reID --name Duke_Reid_16_lr_0.01 --dataset_mode single_Duke --model reid --phase test --gpu 7 --no_dropout
-
-python evaluate_reid.py --dataroot DukeMTMC-reID --name Duke_Reid_16_lr_0.01
